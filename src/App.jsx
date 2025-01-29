8001bad54e868009eac126fc7322a28e3716acc0
erikBr02 <159617849+erikBr02@users.noreply.github.com>
2025-01-29 20:07:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+import axios from "axios";
+import './App.css';
+
+
+function App() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  async function generateAnswer() {
+
+
+    setAnswer("Loading..");
+    try {
+      const response = await axios.post("http://localhost:5173/ask-gemini", {
+        question,
+      });
+      setAnswer(response.data.answer);
+    }
+    catch (error) {
+      console.error('Error fetching response: ', error);
+      setAnswer('Falied to fetch response. Please try again.');
+
+    }
+  }
+
+  return (
+    <>
+      <h1>AI Chatbot</h1>
+      <textarea
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+        cols='15'
+        rows='10'
+      ></textarea>
+      <button onClick={generateAnswer}>Generate Answer</button>
+      <p>{answer}</p>
+    </>
+  )
+}
+
+export default App
