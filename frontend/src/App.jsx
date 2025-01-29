30257447a1c442ac70e6e62b5de5f2b976e54708
erikBr02 <159617849+erikBr02@users.noreply.github.com>
2025-01-30 00:18:11 +0530

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-
 import axios from "axios";
 import './App.css';
+import SpeechToText from './components/SpeechToText';
 
 
 function App() {
@@ -9,35 +9,37 @@ function App() {
   const [answer, setAnswer] = useState("");
 
   async function generateAnswer() {
-
-
+    if(!question.trim()) return; 
     setAnswer("Loading..");
     try {
       const response = await axios.post("http://localhost:3000/ask-gemini", {
-        question,
+        question
       });
       setAnswer(response.data.answer);
     }
     catch (error) {
       console.error('Error fetching response: ', error);
       setAnswer('Falied to fetch response. Please try again.');
-
     }
   }
 
   return (
     <>
       <h1>AI Chatbot</h1>
+      <div style={{display:"flex", alignItems:"center", gap: "10px" }}>
+
       <textarea
         value={question}
         onChange={(e) => setQuestion(e.target.value)}
-        cols='15'
-        rows='10'
-      ></textarea>
+        cols='30'
+        rows='3'
+        ></textarea>
+        <SpeechToText setQuestion={setQuestion} generateAnswer={generateAnswer}/> {/* Using SpeechToText component*/}
+        </div>
       <button onClick={generateAnswer}>Generate Answer</button>
       <p>{answer}</p>
     </>
-  )
+  );
 }
 
 export default App
